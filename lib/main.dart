e714dd6258a6ed128a91bf48bb8b416357625136
Bikrant Dangol <dangolbikrant3@gmail.com>
2025-10-28 15:14:08 +0545

    Doing

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
-// Importing custom providers to manage app-wide state
 import 'package:leave_management_app/providers/leave_provider.dart';  // Manages leave-related data
 import 'package:leave_management_app/providers/user_provider.dart';   // Manages user-related data
 // Importing the login screen UI
 import 'package:leave_management_app/screens/auth/login_screen.dart';
-// Importing the Provider package for state management0
 import 'package:provider/provider.dart';
-/// The main function is the entry point of the app
+
 void main() {
   runApp(MyApp()); // Runs the app with MyApp widget
 }
@@ -19,7 +17,6 @@ class MyApp extends StatelessWidget {
       providers: [
         // Providing UserProvider to manage user-related data across the app
         ChangeNotifierProvider(create: (_) => UserProvider()),
-
         // Providing LeaveProvider to manage leave-related data across the app
         ChangeNotifierProvider(create: (_) => LeaveProvider()),
       ],
